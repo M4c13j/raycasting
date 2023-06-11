d0bdfa689f0f109a6e82730df137bee74e77b277
M4c13j <maciekcx11@gmail.com>
2023-06-12 00:05:03 +0200

    dobre złego początki

---
+++ src/main.cpp
@@ -0,0 +1,26 @@
+#include <raylib.h>
+
+constexpr auto SCREEN_WIDTH  = 800;
+constexpr auto SCREEN_HEIGHT = 450;
+
+int main()
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Window title");
+    SetTargetFPS(60);
+
+
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(BLACK);
+
+        const char* text = "OMG! IT WORKS!";
+
+        EndDrawing();;
+    }
+
+
+    CloseWindow();
+    return 0;
+}
